e016aa097f3a2b0d3a82ffe63c09a2e109a70099
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-02-07 08:15:22 -0500

    feat: proper filesystem ID generation and mostly complete parsing

--- src/parser/cid.rs
+++ src/parser/cid.rs
@@ -16,14 +16,6 @@ impl Cid {
     pub(crate) fn parse(input: &[u8]) -> nom::IResult<&[u8], Self> {
         let (remaining, cid_bytes) = take(CID_LENGTH)(input)?;
 
-        // All zeros and all ones are disallowed, this isn't actually harmful though so we'll only
-        // perform this check in strict mode.
-        if cfg!(feature = "strict")
-            && (cid_bytes.iter().all(|&b| b == 0x00) || cid_bytes.iter().all(|&b| b == 0xff))
-        {
-            return Err(nom::Err::Failure(NomError::new(input, ErrorKind::Verify)));
-        }
-
         let mut bytes = [0u8; CID_LENGTH];
         bytes.copy_from_slice(cid_bytes);
 
--- src/parser/header/filesystem_id.rs
+++ src/parser/header/filesystem_id.rs
@@ -1,4 +1,11 @@
+use nom::bits::bits;
+use nom::bytes::streaming::{tag, take};
+use nom::error::Error as NomError;
+use nom::error::ErrorKind;
+use nom::sequence::tuple;
 use rand::{Rng, RngCore};
+use std::time::SystemTime;
+use uuid::{NoContext, Timestamp, Uuid};
 
 const ID_LENGTH: usize = 16;
 
@@ -6,7 +13,28 @@ pub(crate) struct FilesystemId([u8; ID_LENGTH]);
 
 impl FilesystemId {
     pub(crate) fn generate(rng: &mut impl RngCore) -> Self {
-        let id: [u8; ID_LENGTH] = rng.gen();
-        Self(id)
+        let ts = Timestamp::now(NoContext);
+        let uuid = Uuid::new_v7(ts);
+        Self(uuid.to_bytes_le())
+    }
+
+    pub(crate) fn parse(input: &[u8]) -> nom::IResult<&[u8], Self> {
+        let (remaining, id_bytes) = take(ID_LENGTH)(input)?;
+
+        // All zeros and all ones are disallowed, this isn't actually harmful though so we'll only
+        // perform this check in strict mode.
+        if cfg!(feature = "strict")
+            && (id_bytes.iter().all(|&b| b == 0x00) || id_bytes.iter().all(|&b| b == 0xff))
+        {
+            return Err(nom::Err::Failure(NomError::new(input, ErrorKind::Verify)));
+        }
+
+        // todo(sstelfox): parse into an actually UUID, validate the version, probably store the
+        // UUID instead of the bytes.
+
+        let mut bytes = [0u8; ID_LENGTH];
+        bytes.copy_from_slice(id_bytes);
+
+        Ok((remaining, Self(bytes)))
     }
 }
