d5fb143714737949a1494ce46eeb59db0280973b
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-02-07 10:25:56 -0500

    feat: finish parsers for everything up to the content

--- src/parser/header/format_header.rs
+++ src/parser/header/format_header.rs
@@ -1,4 +1,34 @@
+use nom::bits::bits;
+use nom::bytes::streaming::{tag, take};
+use nom::error::Error as NomError;
+use nom::error::ErrorKind;
+use nom::number::streaming::{le_u32, le_u8};
+use nom::sequence::tuple;
+
+use crate::parser::header::{FilesystemId, IdentityHeader, PublicSettings};
+
 pub(crate) struct FormatHeader {
     ecc_present: bool,
     private: bool,
+    filesystem_id: FilesystemId,
+}
+
+impl FormatHeader {
+    pub(crate) fn parse_with_magic(input: &[u8]) -> nom::IResult<&[u8], Self> {
+        let mut header_parser = tuple((
+            IdentityHeader::parse_with_magic,
+            FilesystemId::parse,
+            PublicSettings::parse,
+        ));
+
+        let (input, (_, filesystem_id, settings)) = header_parser(input)?;
+
+        let header = FormatHeader {
+            ecc_present: settings.ecc_present(),
+            private: settings.private(),
+            filesystem_id,
+        };
+
+        Ok((input, header))
+    }
 }
--- src/parser/header/public_settings.rs
+++ src/parser/header/public_settings.rs
@@ -1,4 +1,46 @@
+use nom::bits::bits;
+use nom::bytes::streaming::{tag, take};
+use nom::error::Error as NomError;
+use nom::error::ErrorKind;
+use nom::number::streaming::{le_u32, le_u8};
+use nom::sequence::tuple;
+
+const ECC_PRESENT_BIT: u8 = 0x02;
+
+const PRIVATE_BIT: u8 = 0x01;
+
+const RESERVED_BITS: u8 = 0xfc;
+
 pub(crate) struct PublicSettings {
     ecc_present: bool,
     private: bool,
+}
+
+impl PublicSettings {
+    pub(crate) fn ecc_present(&self) -> bool {
+        self.ecc_present
+    }
+
+    pub(crate) fn parse(input: &[u8]) -> nom::IResult<&[u8], Self> {
+        let (input, settings_byte) = take(1u8)(input)?;
+        let settings_byte = settings_byte[0];
+
+        if cfg!(feature = "strict") && (settings_byte & RESERVED_BITS) != 0 {
+            return Err(nom::Err::Failure(NomError::new(input, ErrorKind::Verify)));
+        }
+
+        let ecc_present = (settings_byte & ECC_PRESENT_BIT) == ECC_PRESENT_BIT;
+        let private = (settings_byte & PRIVATE_BIT) == PRIVATE_BIT;
+
+        let settings = Self {
+            ecc_present,
+            private,
+        };
+
+        Ok((input, settings))
+    }
+
+    pub(crate) fn private(&self) -> bool {
+        self.private
+    }
 }
