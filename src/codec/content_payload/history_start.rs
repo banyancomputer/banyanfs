53d4166a9c24c8905be209e11035c809ed8659e0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-02-10 13:27:41 -0500

    feat: get history start mechanism in place

--- src/codec/content_payload/history_start.rs
+++ src/codec/content_payload/history_start.rs
@@ -1,9 +1,49 @@
+use async_trait::async_trait;
+use futures::{AsyncWrite, AsyncWriteExt};
+use nom::number::streaming::le_u64;
+use nom::IResult;
+
 use crate::codec::content_payload::{Cid, ContentOptions};
+use crate::codec::AsyncEncodable;
 
 pub struct HistoryStart {
     // todo: replace with vector type when we have it
-    _journal_start_vector: u32,
-    _merkle_root_cid: Cid,
+    journal_start_vector: u64,
+    merkle_root_cid: Cid,
 
-    _content_options: ContentOptions,
+    content_options: ContentOptions,
+}
+
+impl HistoryStart {
+    pub fn parse(input: &[u8]) -> IResult<&[u8], Self> {
+        let (input, journal_start_vector) = le_u64(input)?;
+        let (input, cid) = Cid::parse(input)?;
+        let (input, content_options) = ContentOptions::parse(input)?;
+
+        let history_start = HistoryStart {
+            journal_start_vector,
+            merkle_root_cid: cid,
+            content_options,
+        };
+
+        Ok((input, history_start))
+    }
+}
+
+#[async_trait]
+impl AsyncEncodable for HistoryStart {
+    async fn encode<W: AsyncWrite + Unpin + Send>(
+        &self,
+        writer: &mut W,
+        mut start_pos: usize,
+    ) -> std::io::Result<usize> {
+        let journal_start_vector_bytes = self.journal_start_vector.to_le_bytes();
+        writer.write_all(&journal_start_vector_bytes).await?;
+        start_pos += journal_start_vector_bytes.len();
+
+        let start_pos = self.merkle_root_cid.encode(writer, start_pos).await?;
+        let start_pos = self.content_options.encode(writer, start_pos).await?;
+
+        Ok(start_pos)
+    }
 }
