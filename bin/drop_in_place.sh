12356381097896fc31182b63269bdba539278fb8
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-02-17 14:26:45 -0500

    feat: automate an in place integration with banyan-core's frontend

---
+++ bin/drop_in_place.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GIT_ROOT_DIR="$(git rev-parse --show-toplevel)"
+
+BANYAN_CORE_ROOT_DIR="${GIT_ROOT_DIR}/../banyan-core"
+BANYAN_CORE_FRONTEND_DIR="${BANYAN_CORE_ROOT_DIR}/crates/banyan-core-service/frontend"
+INLINED_TOMB_DIR="${BANYAN_CORE_FRONTEND_DIR}/tomb_build"
+
+(cd ${GIT_ROOT_DIR} && wasm-pack build --debug)
+
+rm -rf ${BANYAN_CORE_FRONTEND_DIR}/node_modules
+rm -rf ${INLINED_TOMB_DIR}/*
+
+cp -f pkg/* ${INLINED_TOMB_DIR}/
+(cd ${INLINED_TOMB_DIR} && jq '.name = "tomb-wasm-experimental"' package.json >tmp.$$.json && mv tmp.$$.json package.json)
+(cd ${BANYAN_CORE_FRONTEND_DIR} && yarn install)
