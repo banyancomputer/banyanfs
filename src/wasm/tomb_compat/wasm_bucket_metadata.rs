af7b97e0a2710e6484bd2f4a850bb38ee40da93f
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-03-07 09:55:45 -0500

    fix: some notes

--- src/wasm/tomb_compat/wasm_bucket_metadata.rs
+++ src/wasm/tomb_compat/wasm_bucket_metadata.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct WasmBucketMetadata;
+pub struct WasmBucketMetadata(ApiDriveId, ApiMetadataId);
 
 #[wasm_bindgen]
 impl WasmBucketMetadata {
@@ -10,14 +10,16 @@ impl WasmBucketMetadata {
         todo!()
     }
 
+    // note(sstelfox): this is a metadata ID
     #[wasm_bindgen(getter)]
     pub fn id(&self) -> String {
         todo!()
     }
 
+    // note(sstelfox): this is wrong, snapshot's aren't guaranteed to be present, this needs t be
+    // an Option, for now we'll return an empty string when not present
     #[wasm_bindgen(getter = snapshotId)]
     pub fn snapshot_id(&self) -> String {
-        // todo: needs to become an option type
         todo!()
     }
 }
