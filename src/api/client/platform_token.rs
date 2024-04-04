008fa615341993abfb1c7a26e8573c848ea129be
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-04-04 10:03:49 -0400

    feat: extract jwt generation so storage hosts can also make use of it

--- src/api/client/platform_token.rs
+++ src/api/client/platform_token.rs
@@ -1,18 +1,10 @@
 use std::sync::Arc;
-use std::time::Duration;
 
 use async_std::sync::RwLock;
-use base64::engine::general_purpose::URL_SAFE_NO_PAD;
-use base64::Engine;
-use rand::RngCore;
-use time::OffsetDateTime;
 
+use crate::api::client::utils::create_jwt;
 use crate::api::client::{ExpiringToken, PLATFORM_AUDIENCE};
 use crate::codec::crypto::SigningKey;
-
-const CLOCK_LEEWAY: Duration = Duration::from_secs(30);
-
-const TOKEN_LIFETIME: Duration = Duration::from_secs(300);
 
 #[derive(Clone, Default)]
 pub(crate) struct PlatformToken(Arc<RwLock<Option<ExpiringToken>>>);
@@ -30,43 +22,8 @@ impl PlatformToken {
             }
         }
 
-        let verifying_key = key.verifying_key();
-        let fingerprint = crate::api::client::utils::api_fingerprint_key(&verifying_key);
-
-        let current_time = OffsetDateTime::now_utc();
-
-        let not_before = current_time - CLOCK_LEEWAY;
-        let expiration = OffsetDateTime::now_utc() + TOKEN_LIFETIME;
-        let not_after = expiration + CLOCK_LEEWAY;
-
-        //let token = jwt_key.sign(claims)?;
-
         let mut rng = crate::utils::crypto_rng();
-        let mut nonce_bytes = [0u8; 24];
-        rng.fill_bytes(&mut nonce_bytes);
-        let nonce = URL_SAFE_NO_PAD.encode(nonce_bytes);
-
-        let jwt_hdr =
-            serde_json::json!({"alg": "ES384", "kid": fingerprint, "typ": "JWT"}).to_string();
-        let jwt_hdr_b64 = URL_SAFE_NO_PAD.encode(jwt_hdr.as_bytes());
-
-        let jwt_claim = serde_json::json!({
-            "iat": current_time.unix_timestamp(),
-            "exp": not_after.unix_timestamp(),
-            "nbf": not_before.unix_timestamp(),
-            "sub": id,
-            "aud": PLATFORM_AUDIENCE,
-            "nonce": nonce,
-        })
-        .to_string();
-        let jwt_claim_b64 = URL_SAFE_NO_PAD.encode(jwt_claim.as_bytes());
-
-        let signed_data = format!("{}.{}", jwt_hdr_b64, jwt_claim_b64);
-        let signature_bytes = key.sign(&mut rng, signed_data.as_bytes()).to_vec();
-        let signature = URL_SAFE_NO_PAD.encode(signature_bytes.as_slice());
-
-        // todo generate signature from private key, base64 encode it
-        let token = format!("{}.{}", signed_data, signature);
+        let (token, expiration) = create_jwt(&mut rng, id, PLATFORM_AUDIENCE, key);
 
         tracing::debug!("generated new platform token");
 
