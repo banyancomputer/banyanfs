3517b57e135f11f5d2c513894f655503bde4c4ee
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-04-18 16:37:57 -0400

    feat: more documentation

--- src/codec/filesystem/block_kind.rs
+++ src/codec/filesystem/block_kind.rs
@@ -6,6 +6,23 @@ use winnow::{
 
 use crate::codec::{ParserResult, Stream};
 
+/// Data blocks come in two varieties, [`BlockKind::IndirectReference`] should only be used when the
+/// data attempting to be stored exceeds the maximum capacity of a single [`FileContent`] struct to
+/// reference directly as [`ContentReference`] (255 children). The full capacity varies based on the
+/// configured [`BlockSize`] in use. At maximum [`BlockSize`] capacity, each [`ContentReference`]
+/// can store $2^255 * 255$ bytes of data (less some overhead).
+///
+/// Blocks at the absolute maximum limit are unwieldly and recommended against. The standard block
+/// sizes defined by this library allows for $2^26 * 255$ which allows individual files up to about
+/// 16GiB, anything below this should generally use a [`BlockKind::Data`]. For larger single file
+/// entries, you can instead use an [`BlockKind::IndirectReference`] block which instead of
+/// containing data, allows increasing the limit of [`ContentLocation`] instances from 255 to the
+/// number that can fit inside the configured [`BlockSize`].
+///
+/// As of right now, a [`BlockSize::small()`] can store 256KiB (ignoring some overhead), and
+/// [`ContentLocation`] Data block references take up 41 bytes allowing 6,393 entries allowing up
+/// to close to 400TiB in a single file entry. More can be used by using additional indirect blocks
+/// or a large block size for the indirect reference block.
 #[derive(Clone, Debug)]
 pub enum BlockKind {
     Data,
