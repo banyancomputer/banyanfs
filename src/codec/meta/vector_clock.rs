a4f5cac8ed1753ab1ded4a8cf44d60425dc9ca8b
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-04-19 17:24:44 -0400

    fix: weird import left in

--- src/codec/meta/vector_clock.rs
+++ src/codec/meta/vector_clock.rs
@@ -2,7 +2,7 @@ use std::cmp::PartialEq;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
 
-use async_std::{fs::read, sync::RwLock};
+use async_std::sync::RwLock;
 use futures::{AsyncWrite, AsyncWriteExt};
 use winnow::{binary::le_u64, Parser};
 
